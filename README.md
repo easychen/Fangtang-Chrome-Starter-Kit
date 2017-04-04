6c0e077b6fd303e84c1cef0ecc89d2eee78b2dac
EasyChen <easychen@qq.com>
2017-04-04 10:15:15 +0800

    init

---
+++ README.md
@@ -0,0 +1,22 @@
+# What
+
+Starter Kit for chrome extension based on Vue2 and ElementUI
+
+# How to use
+
+## build dist js
+
+```
+cd src
+npm install
+npm run build
+```
+
+## load extension
+
+chrome > extentsion > load unpacked extension > ./ext
+
+# demo page
+click extension icon you will see it
+
+
---
+++ src/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1></h1>
+    <el-button @click="visible = true">按钮</el-button>
+    <el-dialog v-model="visible" title="Hello world">
+      <p>欢迎使用 Element</p>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      visible:false
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
